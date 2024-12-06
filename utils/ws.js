04c940b49010f6a66a0e82958e113fd300c81544
David <357195752@qq.com>
2024-12-06 08:48:20 +0800

    ws

--- utils/ws.js
+++ utils/ws.js
@@ -4,46 +4,46 @@ const WebSocket = require("ws");
 const clients = new Map();
 
 // 定义一个函数，用于向所有客户端发送消息
-function broadcast(userId) {
-    const client = clients.get(userId + '')
-    if (client && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({type: 'reply'}));
-    }
+function broadcast(userId, message) {
+  const client = clients.get(userId + "");
+  if (client && client.readyState === WebSocket.OPEN) {
+    client.send(JSON.stringify(message));
+  }
 }
 
 let wss;
 module.exports = {
-    broadcast,
-    wss,
-    createWebSocketServer(server) {
-        wss = new WebSocket.Server({server});
+  broadcast,
+  wss,
+  createWebSocketServer(server) {
+    wss = new WebSocket.Server({ server });
 
-        // WebSocket连接事件
-        wss.on("connection", (ws, req) => {
-            console.log("Client connected");
+    // WebSocket连接事件
+    wss.on("connection", (ws, req) => {
+      console.log("Client connected");
 
-            // 假设用户ID存储在请求的查询参数中
-            const userId = req.url.split('=')[1];
+      // 假设用户ID存储在请求的查询参数中
+      const userId = req.url.split("=")[1];
 
-            // 输出用户ID
-            console.log('User ID:', userId);
+      // 输出用户ID
+      console.log("User ID:", userId);
 
-            // 将客户端添加到集合中
-            clients.set(userId, ws)
+      // 将客户端添加到集合中
+      clients.set(userId, ws);
 
-            ws.on("message", (message) => {
-                console.log(`Received: ${message}`);
-                // 广播消息给所有客户端
-                wss.clients.forEach((client) => {
-                    if (client.readyState === WebSocket.OPEN) {
-                        client.send(message);
-                    }
-                });
-            });
-
-            ws.on("close", () => {
-                console.log("Client disconnected");
-            });
+      ws.on("message", (message) => {
+        console.log(`Received: ${message}`);
+        // 广播消息给所有客户端
+        wss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(message);
+          }
         });
-    },
+      });
+
+      ws.on("close", () => {
+        console.log("Client disconnected");
+      });
+    });
+  },
 };
