632c3d112c172a520adc51ece5f3e873f5c3751b
David <357195752@qq.com>
2024-12-17 16:27:54 +0800

    meilisearch

--- utils/meilisearch.js
+++ utils/meilisearch.js
@@ -1,32 +1,30 @@
-const {MeiliSearch} = require("meilisearch");
+const { MeiliSearch } = require("meilisearch");
 
 const client = new MeiliSearch({
-    host: "http://127.0.0.1:7700",
-    apiKey: "masterKey",
+  host: "http://127.0.0.1:7700",
+  apiKey: "a0wjQECD6E2FBv_FJ0oBeYhKSfpzlb7rmLfb7SBvW_k",
 });
 
-const coursesIndex = client.index("courses")
-coursesIndex.updateSearchableAttributes([
-        'name',
-    ]
-)
+const coursesIndex = client.index("courses");
+(async () => {
+  await coursesIndex.updateSearchableAttributes(["name"]);
+})();
 
-
-const addDocuments = async (documents) => {
-    return await coursesIndex.addDocuments(documents);
+const addDocuments = (documents) => {
+  return coursesIndex.addDocuments(documents);
 };
 
 const deleteDocuments = (documentIds) => {
-    return coursesIndex.deleteDocuments(documentIds);
+  return coursesIndex.deleteDocuments(documentIds);
 };
 
 const search = async (query) => {
-    const search = await coursesIndex.search(query, {
-        attributesToHighlight: ['name'],
-        highlightPreTag: '<span style="color: red">',
-        highlightPostTag: '</span>'
-    });
-    return search
+  const search = await coursesIndex.search(query, {
+    attributesToHighlight: ["name"],
+    highlightPreTag: '<span style="color: red">',
+    highlightPostTag: "</span>",
+  });
+  return search;
 };
 
-module.exports = {addDocuments, deleteDocuments, search};
+module.exports = { addDocuments, deleteDocuments, search };
