af9c1fe34ac66d533299917483499b4c43088119
David <357195752@qq.com>
2024-12-17 15:54:19 +0800

    删除评论

---
+++ utils/verifyPermissions.js
@@ -0,0 +1,12 @@
+const { Unauthorized } = require("http-errors")
+
+const verifyOperate = (data, userId) => {
+    if(userId !== data.userId){
+        throw new Unauthorized("没有权限。");
+    }
+}
+
+
+module.exports = {
+    verifyOperate
+}
